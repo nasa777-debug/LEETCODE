8d5758bda4a85c8bb02f7fe2907d78442fef3146
nasa777-debug <deva02072007@gmail.com>
2026-06-12 06:57:02 +0530

    Time: 0 ms (100%), Space: 19.1 MB (86.78%) - LeetHub

---
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.py
@@ -0,0 +1,10 @@
+class Solution:
+    def transformArray(self, nums: List[int]) -> List[int]:
+        l=[]
+        for i in nums:
+            if i%2==0:
+                l.append(0)
+            elif i%2!=0:
+                l.append(1)
+        l.sort()
+        return l
